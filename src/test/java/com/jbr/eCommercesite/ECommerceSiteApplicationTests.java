1efd562b399c055dac646df5c99f9315afc53e3f
Sean B <strawhorse@gmail.com>
2024-03-17 16:48:55 +0000

    Add files via upload

---
+++ src/test/java/com/jbr/eCommercesite/ECommerceSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbr.eCommercesite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
